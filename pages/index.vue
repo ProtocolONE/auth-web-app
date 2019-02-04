297a95ddbef921cbba36b8e80d8e7470424a92d1
Andrey Solodovnikov <a.solodovnikov@office.ngs.ru>
2019-02-04 12:14:29 +0300

    explicit redirectUri setting AUTH-13

--- pages/index.vue
+++ pages/index.vue
@@ -10,24 +10,29 @@ export default {
 
   data() {
     return {
-      authForm: null
+      authForm: null,
+      redirectUri: 'https://authapp.tst.protocol.one/success/?check=true',
+      isRedirectUriUsed: 'true',
     }
   },
 
   mounted() {
-    this.authForm = new P1AuthWebSdk({
-        clientID: '5c221cde5ffa56fdd05257df',
-        redirectUri: 'https://authapp.tst.protocol.one/success/?check=true'
-        // language: 'en',
-      })
-    this.authForm.on('tokenReceived', (value) => {
-      console.log('tokenReceived', value)
-    });
+
   },
 
   methods: {
     goAuth() {
-      this.authForm.renderModal();
+      const authForm = new P1AuthWebSdk({
+          clientID: '5c221cde5ffa56fdd05257df',
+          redirectUri: this.isRedirectUriUsed === 'true' ? this.redirectUri : '',
+          // language: 'en',
+        })
+      authForm.on('tokenReceived', (value) => {
+        console.log(11111, 'token', value)
+        alert(value);
+      });
+
+      authForm.renderModal();
     }
   }
 }
@@ -36,6 +41,24 @@ export default {
 <template>
   <div>
     <h1>Hello</h1>
+    <div>
+      <h4>Use redirect URI?</h4>
+      <label>
+        <input type="radio" value="true" v-model="isRedirectUriUsed">
+        Yes
+      </label>
+      <label>
+        <input type="radio" value="false" v-model="isRedirectUriUsed">
+        No
+      </label>
+    </div>
+    <div>
+      <pre>{{isRedirectUriUsed}}</pre>
+      <h4>Redirect URI</h4>
+      <input type="text" v-model="redirectUri" :disabled="isRedirectUriUsed === 'false'" style="width: 100%">
+
+    </div>
+    <br>
     <button @click="goAuth">Auth me</button>
   </div>
 </template>
